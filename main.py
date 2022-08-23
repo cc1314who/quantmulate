3927fc84dfa35d5566daf60122fcd109c0758ac4
cc1314who <87588639+cc1314who@users.noreply.github.com>
2022-08-23 09:26:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 import os
 import random
 
-today = datetime.now()+timedelta(hours=8)
+#today = datetime.now()+timedelta(hours=8)
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
@@ -18,43 +18,9 @@ user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
-def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+today = date.today()  #获取今天的日期
+my_birthday = bir
+if my_birthday < today:#如果你今年的生日过了，加到下一年
+    my_birthday = my.replace(year=today.year+1)
+time_to_birthday = abs(my - today) #算一下这两天差了多少时间戳
 
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
-
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
-
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
-
-def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
-
-
-client = WeChatClient(app_id, app_secret)
-
-wm = WeChatMessage(client)
-wea, temperature = get_weather()
-
-data = {
-"today":{"value":today,"color":get_random_color()},
-"weather":{"value":wea, "color":get_random_color()},
-"temperature":{"value":temperature, "color":get_random_color()},
-"love_days":{"value":get_count(), "color":get_random_color()},
-"birthday_left":{"value":get_birthday(),"color":get_random_color()},
-"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
